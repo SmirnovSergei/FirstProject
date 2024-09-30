3668853e238f9281106ec53fadc9fc6bca70f249
Smirnov Sergey <fost@live.ru>
2024-09-30 22:58:43 +0300

    Задание к 16-му уроку.

--- NewProject/ViewController.swift
+++ NewProject/ViewController.swift
@@ -5,14 +5,8 @@ class ViewController: UIViewController {
     private let helper = Helper()
     private let userRepository = UserRepository()
     private let textLabel = UILabel()
-    private let customButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("Show FullName", for: .normal)
-        button.setTitleColor(.black, for: .normal)
-        button.backgroundColor = .green
-        button.frame = CGRect(x: 100, y: 150, width: 150, height: 50)
-        return button
-    }()
+    private let customButton = UIButton()
+    private let stackView = UIStackView()
         
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,8 +16,12 @@ class ViewController: UIViewController {
         updateTeam()
         
         setupLabel()
-        view.addSubview(textLabel)
-        view.addSubview(customButton)
+        setupButton()
+        setupStackView()
+//        view.addSubview(textLabel)
+//        view.addSubview(customButton)
+        view.addSubview(stackView)
+        setupLayout()
     }
     
     private func updateTeam() {
@@ -39,7 +37,35 @@ class ViewController: UIViewController {
         textLabel.text = randomUser
         textLabel.font = .systemFont(ofSize: 25)
         textLabel.textColor = .blue
-        textLabel.frame = CGRect(x: 100, y: 100, width: 300, height: 50)
+        textLabel.textAlignment = .center
+//        textLabel.frame = CGRect(x: 100, y: 100, width: 230, height: 50)
+    }
+        
+    private func setupButton() {
+        customButton.setTitle("Show FullName", for: .normal)
+        customButton.setTitleColor(.black, for: .normal)
+        customButton.backgroundColor = .green
+//        customButton.frame = CGRect(x: 100, y: 150, width: 150, height: 50)
+    }
+    
+    private func setupStackView() {
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.alignment = .fill
+        
+        stackView.addArrangedSubview(textLabel)
+        stackView.addArrangedSubview(customButton)
+    }
+    
+    private func setupLayout() {
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.widthAnchor.constraint(equalToConstant: 230),
+            stackView.heightAnchor.constraint(equalToConstant: 100)
+        ])
     }
 }
 
